c952e7309131c65367287a978fa68510c022ff7a
eleme <eleme@wyw-eleme-mbp.local>
2021-05-20 11:43:22 +0800

    init another repo delete package

---
+++ RunAStarSearch.java
@@ -0,0 +1,42 @@
+//package search.api;
+
+public class RunAStarSearch {
+    public static void main(String[] arg) {
+//        TerrainMap tm = new TerrainMap("C:\\Users\\weiyaowu\\IdeaProjects\\hello\\src\\com\\company\\diablo.pgm");
+        TerrainMap tm = new TerrainMap("G:\\ml\\SearchAlgorithm3\\src\\search\\api\\tmc.pgm");
+        int[][] tmap = tm.getTmap();
+
+
+        Coords start = new Coords(7, 0);
+        Coords end = new Coords(tm.getDepth() - 1, tm.getWidth() - 1);
+
+        Coords end1 = new Coords(5, 8);
+        System.out.println(tm.getHeight() - 1);
+        System.out.println(tm.getWidth() - 1);
+
+        SearchState initState = new SearchStateAStarCost(0, start);
+        SearchState goalState = new SearchStateAStarCost(0, end);
+
+        AStarSearch AStarSearch = new AStarSearch();
+        AStarSearch.runSearchAStar(initState, goalState, tm, "AStar", "Manhattan");
+        double efficiency_MD = AStarSearch.efficiency;
+
+        AStarSearch.runSearchAStar(initState, goalState, tm, "AStar", "Euclidean");
+        double efficiency_ED = AStarSearch.efficiency;
+
+        AStarSearch.runSearchAStar(initState, goalState, tm, "AStar", "HeightDifference");
+        double efficiency_HD = AStarSearch.efficiency;
+
+        AStarSearch.runSearchAStar(initState, goalState, tm, "AStar", "ManhattanWithHeight");
+        double efficiency_MDHD = AStarSearch.efficiency;
+
+        AStarSearch.runSearchAStar(initState, goalState, tm, "AStar", "EuclideanWithHeight");
+        double efficiency_EDHD = AStarSearch.efficiency;
+
+        System.out.println("efficiency_MD " + efficiency_MD);
+        System.out.println("efficiency_ED " + efficiency_ED);
+        System.out.println("efficiency_HD " + efficiency_HD);
+        System.out.println("efficiency_MDHD " + efficiency_MDHD);
+        System.out.println("efficiency_EDHD " + efficiency_EDHD);
+    }
+}
---
+++ SearchStateAStarCost.java
@@ -0,0 +1,148 @@
+//package search.api;
+
+import java.util.ArrayList;
+
+public class SearchStateAStarCost extends SearchState {
+    public SearchStateAStarCost(int lcost, Coords point) {
+        this.localCost = lcost;
+        this.point = point;
+    };
+    public SearchStateAStarCost(int lcost, Coords point, int estRemCost) {
+        this.localCost = lcost;
+        this.point = point;
+        this.estRemCost = estRemCost;
+    };
+    public boolean goalPredicate(Search searcher) {
+        if (searcher.currentNode.sameState(searcher.goalNode)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    public ArrayList<SearchState> getSuccessors(Search searcher) {
+        ArrayList<SearchState> successorStates = new ArrayList<SearchState>();
+        Coords curStatePoint = searcher.currentNode.get_State().point;
+        Coords goalStatePoint = searcher.goalNode.get_State().point;
+        String AStarHCostMethod = searcher.AStarHCost;
+
+        int w = searcher.tmp.getWidth();
+        int h = searcher.tmp.getDepth();
+        int [][] tmpHeights = searcher.tmp.getTmap();
+
+        int x = curStatePoint.getx();
+        int y = curStatePoint.gety();
+        int cost;
+        int HCost;
+
+        if (x < w - 1) {
+            Coords tempCoord = new Coords(y, x + 1);
+            cost = 1;
+            if (tmpHeights[y][x + 1] > tmpHeights[y][x]) {
+                cost += Math.abs(tmpHeights[y][x + 1] - tmpHeights[y][x]);
+            }
+
+            HCost = HCostCompute(tempCoord, goalStatePoint, tmpHeights, AStarHCostMethod);
+            SearchState tmpState = new SearchStateAStarCost(cost, tempCoord, HCost);
+            successorStates.add(tmpState);
+        }
+
+        if (y < h - 1) {
+            Coords tempCoord = new Coords(y + 1, x);
+            cost = 1;
+            if (tmpHeights[y + 1][x] > tmpHeights[y][x]) {
+                cost += Math.abs(tmpHeights[y][x + 1] - tmpHeights[y][x]);
+            }
+
+            HCost = HCostCompute(tempCoord, goalStatePoint, tmpHeights, AStarHCostMethod);
+            SearchState tmpState = new SearchStateAStarCost(cost, tempCoord, HCost);
+            successorStates.add(tmpState);
+        }
+
+        if (x > 0) {
+            Coords tempCoord = new Coords(y, x - 1);
+            cost = 1;
+            if (tmpHeights[y][x - 1] > tmpHeights[y][x]) {
+                cost += Math.abs(tmpHeights[y][x - 1] - tmpHeights[y][x]);
+            }
+
+            HCost = HCostCompute(tempCoord, goalStatePoint, tmpHeights, AStarHCostMethod);
+            SearchState tmpState = new SearchStateAStarCost(cost, tempCoord, HCost);
+            successorStates.add(tmpState);
+        }
+
+        if (y > 0) {
+            cost = 1;
+            Coords tempCoord = new Coords(y - 1 , x);
+            if (tmpHeights[y - 1][x] > tmpHeights[y][x]) {
+                cost += Math.abs(tmpHeights[y - 1][x] - tmpHeights[y][x]);
+            }
+
+            HCost = HCostCompute(tempCoord, goalStatePoint, tmpHeights, AStarHCostMethod);
+            SearchState tmpState = new SearchStateAStarCost(cost, tempCoord, HCost);
+            successorStates.add(tmpState);
+        }
+
+        return successorStates;
+    };
+
+    public boolean sameState(SearchState n2) {
+        if (this.point.getx() == n2.point.getx() && this.point.gety() == this.point.gety()) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    // heuristics distances
+
+    // Manhattan Distance
+    public int ManhattanDistance(Coords curPoint, Coords goalPoint) {
+        return Math.abs(curPoint.getx() - goalPoint.getx()) + Math.abs(curPoint.gety() - goalPoint.gety());
+    }
+
+    // Euclidean Distance
+    public int EuclideanDistance(Coords curPoint, Coords goalPoint) {
+        return (int)Math.ceil(Math.sqrt(Math.pow((curPoint.getx() - goalPoint.getx()), 2) +
+                Math.pow((curPoint.gety() - goalPoint.gety()), 2)));
+    }
+
+    // Height Difference
+    public int HeightDifference(Coords curPoint, Coords goalPoint, int[][] tmpHeights) {
+        int curHeight = tmpHeights[curPoint.gety()][curPoint.getx()];
+        int goalHeight = tmpHeights[goalPoint.gety()][goalPoint.getx()];
+        int HCost = 1;
+        if (curHeight < goalHeight) {
+            HCost = goalHeight - curHeight;
+        }
+        return HCost;
+    }
+
+    // Manhattan Distance with Height Difference
+    public int ManhattanWithHeight(Coords curPoint, Coords goalPoint, int[][] tmpHeights) {
+        return ManhattanDistance(curPoint, goalPoint) + HeightDifference(curPoint, goalPoint, tmpHeights);
+    }
+
+    // Euclidean Distance with Height Difference
+    public int EuclideanWithHeight(Coords curPoint, Coords goalPoint, int[][] tmpHeights) {
+        return EuclideanDistance(curPoint, goalPoint) + HeightDifference(curPoint, goalPoint, tmpHeights);
+    }
+
+    public int HCostCompute(Coords curPoint, Coords goalPoint, int[][] tmpHeights, String HCostMethod) {
+        if (HCostMethod == "Manhattan") {
+            return ManhattanDistance(curPoint, goalPoint);
+        } else if (HCostMethod == "Euclidean") {
+            return EuclideanDistance(curPoint, goalPoint);
+        } else if (HCostMethod == "HeightDifference") {
+            return  HeightDifference(curPoint, goalPoint, tmpHeights);
+        } else if (HCostMethod == "ManhattanWithHeight") {
+            return ManhattanWithHeight(curPoint, goalPoint, tmpHeights);
+        } else if (HCostMethod == "EuclideanWithHeight") {
+            return EuclideanWithHeight(curPoint, goalPoint, tmpHeights);
+        } else {
+            return 0;
+        }
+
+    }
+
+}
